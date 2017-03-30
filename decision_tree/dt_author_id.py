de03a8bd1a68abf06a3b9bbbd11f9459d1e5099e
the-dagger <harshithdwivedi@gmail.com>
2017-03-31 02:58:44 +0530

    feat : implement prediction using decision trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -7,25 +7,37 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import tree
 
+# clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
+print len(features_train[0])
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
+
+t1 = time()
+result = clf.predict(features_test)
+print "training time:", round(time() - t1, 3), "s"
+
+from sklearn.metrics import accuracy_score
+
+print accuracy_score(labels_test, result)
 
 #########################################################
 ### your code goes here ###
 
 
 #########################################################
-
-
