2077e5942b73060bd2fcfd11dacbeeb31c58f5d2
the-dagger <harshithdwivedi@gmail.com>
2017-04-03 21:15:57 +0530

    feat : implement prediction using KNN

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -24,20 +24,23 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
+# plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
 
+clf = KNeighborsClassifier(n_neighbors=8)
 
+clf.fit(features_train,labels_train)
 
+result = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
 
-
-
-
+print accuracy_score(labels_test,result)
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
